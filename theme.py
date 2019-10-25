fe672a125fa23681ecd780c5b1625d98016a393a
rektrex <23721797+rektrex@users.noreply.github.com>
2019-10-26 05:04:08 +0530

    add highlight definition, and division into sections

--- theme.py
+++ theme.py
@@ -1,7 +1,37 @@
-# name of the colorscheme. The resultant colorscheme is written to {name}.vim
-name = 'truelove'
-
 import utils
 
+name                = 'truelove'
+style               = 'dark'
+
+# colors
+cursor              = '#c8c8c8'
+background          = '#212121'
+foreground          = '#dfdfdf'
+foregroundFaded     = '#6f6f6f'
+brown               = 'BROWN'
+yellow              = '#dfc56d'
+gray                = '#373737'
+green               = '#88c563'
+red                 = '#e76d6d'
+accent              = '#ff6188'
+
+highlightGroups = [
+        ('Normal', {'bg': background, 'fg': foreground}),
+        ('_new', 'statusline'),
+]
+
 with open(f'{name}.vim', 'w') as f:
-    f.write(utils.init(name))
+    f.write(utils.init(name, style))
+
+    for name, group in highlightGroups:
+        if name == '_new':
+            f.write('\n\n' + utils.addNewSection(group) + '\n')
+            continue
+
+        bg = group.get('bg', 'NONE')
+        fg = group.get('fg', 'NONE')
+        gui = group.get('gui', 'NONE')
+
+        f.write('\n' + utils.setHighlight(name, bg, fg, gui))
+
+    f.write('\n' + utils.statusline)
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-def init(name):
+def init(name, background = 'dark'):
     out = ""
     out += \
 f"""hi clear
@@ -8,7 +8,52 @@ if exists('syntax_on')
 endif
 
 let g:colors_name='{name}'
-
+set background={background}
 """
 
     return out
+
+
+statusline =\
+"""if g:micro_theme_statusline==1
+    set statusline=
+    set statusline+=\ %{StatuslineMode()}
+    set statusline+=%=
+    if g:micro_coc_status==1
+        set statusline+=%{coc#status()}\ 
+    endif
+    set statusline+=\ %t
+    set statusline+=%m
+    set statusline+=%r
+    set laststatus=2
+
+    function! StatuslineMode()
+        let l:mode=mode()
+        if l:mode==#"n"
+            return "NORMAL"
+        elseif l:mode==?"v"
+            return "VISUAL"
+        elseif l:mode==#"i"
+            return "INSERT"
+        elseif l:mode==#"R"
+            return "REPLACE"
+        elseif l:mode==?"s"
+            return "SELECT"
+        elseif l:mode==#"t"
+            return "TERMINAL"
+        elseif l:mode==#"c"
+            return "COMMAND"
+        elseif l:mode==#"!"
+            return "SHELL"
+        endif
+    endfunction
+endif
+"""
+
+
+def setHighlight(name , bg, fg, gui):
+    return f'hi {name} guibg={bg} guifg={fg} gui={gui}'
+
+
+def addNewSection(group):
+    return '" ' + group.ljust(75, '-')
